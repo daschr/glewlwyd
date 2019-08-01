9d8887a5aef3f53b6ff3ecd55116b5855d7f33e3
David Schramm <david.schramm@live.com>
2019-08-01 15:42:40 +0200

    first try

--- src/user/cusdb.c
+++ src/user/cusdb.c
@@ -27,7 +27,7 @@
 #include <jansson.h>
 #include <yder.h>
 #include "../glewlwyd-common.h"
-#include <libpq/pqcomm.h>
+#include </usr/include/postgresql/libpq-fe.h>
 
 #define USER_REPL "{{username}}"
 #define HASH_REPL "{{hash}}"
@@ -41,13 +41,13 @@ char *user_repl=USER_REPL;
 char *hash_repl=HASH_REPL;
 
 //holds db connection
-PGConn *pg_connection=NULL;
-char **pg_keywords=NULL;
-char **pg_values=NULL;
+PGconn *db_connection=NULL;
+char pg_keywords[12][128];
+char pg_values[12][128];
 
 
 
-int str_repl(char *buffer, size_t bufsize,char *string, char *tbstring, char *replstring){
+int str_repl(char *buffer, size_t bufsize,const char *string,const char *tbstring,const char *replstring){
 	size_t strpos=0;
 	size_t bufferpos=0;
 	size_t tbstringl=strlen(tbstring);
@@ -71,15 +71,7 @@ int str_repl(char *buffer, size_t bufsize,char *string, char *tbstring, char *re
 	return 1;
 }
 
-int in_array(char **a,char *s){
-	for(size_t i=0;i<sizeof(a)/sizeof(*a);++i){
-		if(strcmp(a[i],s)==0)
-			return 1;
-	}
-	return 0;
-}
-
-int map_digest(char *s){
+int map_digest(const char *s){
 	EQ("MD5")
 		return digest_MD5;
 	else EQ("SSHA1")
@@ -87,7 +79,7 @@ int map_digest(char *s){
 	else EQ("SHA1")
 		return digest_SHA1;
 	else EQ("SSHA224")
-		return digest_SSA224;
+		return digest_SSHA224;
 	else EQ("SHA224")
 		return digest_SHA224;
 	else EQ("SSHA256")
@@ -110,7 +102,7 @@ int map_digest(char *s){
 }
 
 int connect_db(void){
-	db_connection=PQconnectdbParams(pg_keywords,pg_values,0);
+	db_connection=PQconnectdbParams((const char * const*)pg_keywords,(const char* const *)pg_values,0);
 	
 	if(db_connection == NULL)
 		return 0;
@@ -172,27 +164,20 @@ json_t * user_module_load(struct config_module * config) {
                        "type",
                        "string",
                        "mandatory",
-                       json_true(),
+                       json_true()
 		     );
 }
 
 int user_module_unload(struct config_module * config) {
   UNUSED(config);
   PQfinish(db_connection);
-  for(size_t i=0;i<sizeof(pg_keywords)/sizeof(char*);++i){
-  	free(pg_keywords[i]);
-	free(pg_values[i]);
-  }
-  free(pg_keywords);
-  free(pg_values);
   return G_OK;
 }
 
 json_t * user_module_init(struct config_module * config, int readonly, json_t * j_params, void ** cls) {
   UNUSED(config);
   UNUSED(readonly);
-  size_t index = 0;
-  json_t * j_element = NULL, * j_return = NULL;
+  json_t * j_return = NULL;
   int ret;
   char *opts[]={"host","dbname","sql_cmd","hash_type"};
   char *db_opts[]={"host","dbname","sslmode","user","password"};
@@ -209,13 +194,13 @@ json_t * user_module_init(struct config_module * config, int readonly, json_t * 
     }
     
     if(!json_is_integer(json_object_get(j_params,"port")) || json_integer_value(json_object_get(j_params,"port")) <=0){
-    		sprintf(emsg,"user_module_init cusdb - parameter port is mandatory and must be a positive number",opts[i]);
+    		sprintf(emsg,"user_module_init cusdb - parameter port is mandatory and must be a positive number");
 		y_log_message(Y_LOG_LEVEL_ERROR, emsg);
 		j_return = json_pack("{sis[s]}", "result", G_ERROR_PARAM, "error", emsg);
 		ret = G_ERROR_PARAM;
     }
 
-    if(map_digest(json_string_value(json_object_get(jparams,"hash_type"))) == -1) {
+    if(map_digest(json_string_value(json_object_get(j_params,"hash_type"))) == -1) {
     		sprintf(emsg,"user_module_init cusdb - parameter must be an cipher");
 		y_log_message(Y_LOG_LEVEL_ERROR, emsg);
 		j_return = json_pack("{sis[s]}", "result", G_ERROR_PARAM, "error", emsg);
@@ -232,22 +217,18 @@ json_t * user_module_init(struct config_module * config, int readonly, json_t * 
   if (ret == G_OK) {
     *cls = json_incref(j_params);
   }
-  pg_keywords=malloc(sizeof(char*));
-  pg_values=malloc(sizeof(char*));
-  size_t cur_size=1;
-
+  size_t cur_pos=0;
+  char num[32];
   for(size_t i=0;i<sizeof(db_opts)/sizeof(char *);++i){
 	if(!json_string_length(json_object_get(j_params,db_opts[i])))
 		continue;
-	if(cur_size==i){
-		pg_keywords=realloc(pg_keywords,++cur_size*sizeof(char*));
-		pg_values=realloc(pg_keywords,++cur_size*sizeof(char*));
-	}
-	pg_keywords[i]=strdup(db_opts[i]);
-	if(strcmp(pg_keywords[i],"port")==0)
-		pg_values[i]=strdup(atoi(json_integer_value(json_object_get(j_params,db_opts[i]))));
-	else
-		pg_values[i]=strdup(json_string_value(json_object_get(j_params,db_opts[i])));
+	strncpy(pg_keywords[cur_pos],db_opts[i],127);
+	if(strcmp(pg_keywords[i],"port")==0){
+		sprintf(num,"%lld",json_integer_value(json_object_get(j_params,db_opts[i])));
+		strncpy(pg_values[cur_pos],num,127);
+	
+	}else
+		strncpy(pg_values[cur_pos],json_string_value(json_object_get(j_params,db_opts[i])),127);
   }
  
   if(!connect_db()){
@@ -338,7 +319,7 @@ int user_module_check_password(struct config_module * config, const char * usern
   UNUSED(config);
 
   if(PQstatus(db_connection) != CONNECTION_OK)
-	if(!db_connect())
+	if(!connect_db())
 		return G_ERROR;
 
   json_t *conf=(json_t *) cls;
@@ -348,15 +329,15 @@ int user_module_check_password(struct config_module * config, const char * usern
 
 
   if(!str_repl(cmd_buffer1,BUFSIZE,json_string_value(json_object_get(conf,"sql_cmd")),user_repl,username))
-  	return G_UNAUTHORIZED;
-  if(!str_repl(cmd_buffer2,BUFSIZE,cmd_buffer2,user_repl,username))
-  	return G_UNAUTHORIZED;
+  	return G_ERROR_UNAUTHORIZED;
+  if(!str_repl(cmd_buffer2,BUFSIZE,cmd_buffer2,hash_repl,hash))
+  	return G_ERROR_UNAUTHORIZED;
   PGresult *res=PQexec(db_connection,cmd_buffer2);
 
   if(PQresultStatus(res) == PGRES_COMMAND_OK)
   	return G_OK;
-  else
-  	return G_UNAUTHORIZED;
+  return G_ERROR_UNAUTHORIZED;
+
 }
 
 int user_module_update_password(struct config_module * config, const char * username, const char * new_password, void * cls) {
